0e562988ce7df9309511b540050a3a6957d44cdb
italianodevops <gopycodewar@gmail.com>
2026-07-03 06:51:28 +0200

    Test ecr login

--- jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
+++ jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
@@ -3,13 +3,13 @@
 pipeline {
     agent {node 'jenkins-node' }
     stages {
-        stage('test'){
+        stage('test login ecr'){
             steps{
                 withAWS(region:'us-east-1',credentials:'pet-shop-jenkins-ci-keys'){
                     script{
-                        sh """
-                            aws ecr describe-repositories
-                        """
+                        def login = ecrLogin()
+
+                        sh login
                     }
                 }
             }
