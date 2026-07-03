98382422f5fb8af2598326058f3a49bce98e86be
italianodevops <gopycodewar@gmail.com>
2026-07-03 07:50:39 +0200

    assume-role added

--- jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
+++ jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
@@ -10,10 +10,10 @@ pipeline {
     stages {
         stage('test login ecr'){
             steps{
-                sh('
+                sh('''
                 aws sts assume-role --role-arn "arn:aws:iam::013484737363:role/pet-shop-jenkins-ci-role" --role-session-name test-session --no-cli-pager
                 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin
-                ')
+                ''')
                 }
             }
         }
