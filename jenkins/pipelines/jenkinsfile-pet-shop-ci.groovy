20d5e6eb126096d57fcbcec62f6b3ef9a5ee5b05
italianodevops <gopycodewar@gmail.com>
2026-07-03 09:04:06 +0200

    Build and push image

--- jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
+++ jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
@@ -6,14 +6,34 @@ pipeline {
        AWS_ACCESS_KEY_ID = credentials('pet-shop-ci-access-key-id')
        AWS_SECRET_ACCESS_KEY = credentials('SecretAccessKey-pet-shop-jenkins-ci')
        AWS_DEFAULT_REGION='us-east-1'
+       ECR_REGISTRY = '013484737363.dkr.ecr.us-east-1.amazonaws.com'
+       ECR_REPO = 'prod_petshop_project'
+       IMAGE_VERSION = "${env.BUILD_NUMBER}"
     }
     stages {
-        stage('test login ecr'){
+        stage('Build'){
+            dir('./app-pet-shop/pet-shop'){
+                sh('''
+                docker build -t ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_VERSION} .
+                ''')
+            }
+            
+        }
+        stage('Login Ecr'){
             steps{
                 sh('''              
-                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 013484737363.dkr.ecr.us-east-1.amazonaws.com
+                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR_REGISTRY}
                 ''')
                 }
+        }
+        stage('Push'){
+            steps{
+                sh('''              
+                docker push ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_VERSION}
+                ''')
             }
         }
+    }
 }
+        
+        
