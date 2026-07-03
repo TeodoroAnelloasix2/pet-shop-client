fa7a32f84f59207a1941d67ba52e7069c1e85f85
italianodevops <gopycodewar@gmail.com>
2026-07-03 07:59:21 +0200

    attached permission to user

--- jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
+++ jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
@@ -10,13 +10,7 @@ pipeline {
     stages {
         stage('test login ecr'){
             steps{
-                sh('''
-                CREDS=$(aws sts assume-role --role-arn "arn:aws:iam::013484737363:role/pet-shop-jenkins-ci-role" --role-session-name test-session --no-cli-pager)
-    
-                export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r '.Credentials.AccessKeyId')
-                export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r '.Credentials.SecretAccessKey')
-                export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.Credentials.SessionToken')
-                
+                sh('''              
                 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin
                 ''')
                 }
