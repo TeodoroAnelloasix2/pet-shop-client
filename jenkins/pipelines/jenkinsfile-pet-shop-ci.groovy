a7f917147e6fa8bae88e3e80a953aece1a9be699
italianodevops <gopycodewar@gmail.com>
2026-07-03 08:09:39 +0200

    It's better with ecr registry :P

--- jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
+++ jenkins/pipelines/jenkinsfile-pet-shop-ci.groovy
@@ -11,7 +11,7 @@ pipeline {
         stage('test login ecr'){
             steps{
                 sh('''              
-                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin
+                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 013484737363.dkr.ecr.us-east-1.amazonaws.com
                 ''')
                 }
             }
