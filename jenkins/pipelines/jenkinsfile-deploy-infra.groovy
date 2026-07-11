149c475100fcc90a1b00d6f7c7add64cc226eda5
italianodevops <gopycodewar@gmail.com>
2026-07-11 11:27:20 +0200

    terraform.tfvars uploaded, secret file created

--- jenkins/pipelines/jenkinsfile-deploy-infra.groovy
+++ jenkins/pipelines/jenkinsfile-deploy-infra.groovy
@@ -7,7 +7,6 @@ pipeline{
         AWS_SECRET_ACCESS_KEY = credentials('jenkins-user-secret-access-key')
         AWS_DEFAULT_REGION='us-east-1'
         INFRA_VERSION = "${env.BUILD_NUMBER}"
-        TFVARS_FILE=
     }
     parameters{
         booleanParam(name: 'autoApprove', defaultValue: false, description: 'Automatically run apply after generating plan?')
