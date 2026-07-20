799efd6925d06d2344bca95964e8495a546dcfc3
italianodevops <gopycodewar@gmail.com>
2026-07-20 09:38:05 +0200

    Feat: pipeline to create eks cluster, CD step

--- jenkins/pipelines/jenkinsfile-pet-shop-cd.groovy
+++ jenkins/pipelines/jenkinsfile-pet-shop-cd.groovy
@@ -21,6 +21,7 @@ pipeline{
             steps{
                 dir('eks/overlays/prod'){
                 sh('''
+                #!/bin/bash
                 mapfile -t ecr_tags < <(aws ecr list-images --repository-name prod_petshop_project --filter tagStatus=TAGGED,imageStatus=ACTIVE  --query 'imageIds[*].imageTag' --output json | jq -r '.[]')
                 last_tag=${ecr_tags[-1]}
                 kustomize edit set image pet-shop-to-customize=013484737363.dkr.ecr.us-east-1.amazonaws.com/prod_petshop_project:${last_tag}
