a09b68da8c37d706974a4fb8f2a8c475de022183
italianodevops <gopycodewar@gmail.com>
2026-07-25 14:21:38 +0200

    Get last tag dinamically

--- jenkins/pipelines/jenkinsfile-pet-shop-cd.groovy
+++ jenkins/pipelines/jenkinsfile-pet-shop-cd.groovy
@@ -22,7 +22,7 @@ pipeline{
                 dir('eks/overlays/prod'){
                 sh('''
                 #!/bin/bash
-                last_tag=$(aws ecr list-images --repository-name prod_petshop_project --filter tagStatus=TAGGED,imageStatus=ACTIVE  --query 'imageIds[*].imageTag' --output json | jq -r '.[-1]')
+                last_tag=$(aws ecr list-images --repository-name prod_petshop_project --filter tagStatus=TAGGED,imageStatus=ACTIVE   --query "sort_by(imageIds, &imageTag)[-1].imageTag"  --output text)
                 kustomize edit set image pet-shop-to-customize=013484737363.dkr.ecr.us-east-1.amazonaws.com/prod_petshop_project:${last_tag}
                 ''')
                 }
