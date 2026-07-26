d67a9b2f708992f656436ab17b310bee8d34dd0f
italianodevops <gopycodewar@gmail.com>
2026-07-26 10:55:49 +0200

    Feat: create table before starting http server

--- app-pet-shop/pet-shop/internal/awsrds/queries.go
+++ app-pet-shop/pet-shop/internal/awsrds/queries.go
@@ -2,9 +2,9 @@ package awsrds
 
 var (
 	CreateTableQuery = `CREATE TABLE users(
-		Name varchar(100);
-		Surname varchar(100);
-		Email varchar(100);
-		Password varchar(255);
-	)`
+		Name varchar(100),
+		Surname varchar(100),
+		Email varchar(100),
+		Password varchar(255)
+	);`
 )
