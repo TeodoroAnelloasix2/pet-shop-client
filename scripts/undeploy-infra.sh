37237e3c817adb63470cab3b025b6b122c2555e7
italianodevops <gopycodewar@gmail.com>
2026-07-26 17:16:29 +0200

    Feat: Developing terraform destroy pipeline

--- scripts/undeploy-infra.sh
+++ scripts/undeploy-infra.sh
@@ -32,8 +32,9 @@ Delete_alb(){
 Delete_images(){
     echo "Deleting images"
     ecr_arn=$(aws ecr describe-repositories --no-cli-pager --region "${aws_region}" --query "${ecr_query}" --output text)
-    image_list=$(aws ecr list-images --repository-name "$ecr_repository"  --region "$aws_region" --no-cli-pager  --output json)
+    image_list=$(aws ecr list-images --repository-name "$ecr_repository"  --region "$aws_region" --no-cli-pager  --output json  --query 'imageIds[*].imageDigest')
     img_num=$(echo "$image_list" | jq '.[] | length')
+    image_ids=$(echo "$image_digests" | jq '[.[] | {imageDigest: .}]')
     if [[ ! -z "ecr_arn" ]];then 
         echo -e "Deleting images from repository \n ${ecr_arn}"
         if [[ "$img_num" -gt 0 ]];then
